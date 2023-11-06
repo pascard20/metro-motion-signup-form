463df2ef6c4a5156130a1c9f0eb4e7205b16a44a
pascard20 <141923751+pascard20@users.noreply.github.com>
2023-11-06 02:47:26 +0100

    Handle password confirmation

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 elemForm = document.querySelector('.form');
+elemPassword = document.querySelector('#pwd');
+elemPasswordConfirm = document.querySelector('#pwd-confirm');
 
 const handleInput = event => {
   const element = event.target;
   if (!element.required && !element.value) element.classList.add('empty');
+  if ([elemPassword, elemPasswordConfirm].includes(element)) {
+    if (elemPassword.value !== elemPasswordConfirm.value) {
+      elemPasswordConfirm.classList.add('invalid');
+    } else elemPasswordConfirm.classList.remove('invalid');
+  }
 }
 
 const handleFocusOut = event => {
