176edd8f4ca6bb987b593d12361ec0b8f8d5fb37
pascard20 <141923751+pascard20@users.noreply.github.com>
2023-11-06 12:52:47 +0100

    Introduce password requirements

--- script.js
+++ script.js
@@ -1,21 +1,79 @@
 elemForm = document.querySelector('.form');
 elemPassword = document.querySelector('#pwd');
 elemPasswordConfirm = document.querySelector('#pwd-confirm');
+elemPasswordConstraints = document.querySelector('.pwd-constraints');
+
+elemPwdItemLength = document.querySelector('.pwd-constraints__item.length');
+elemPwdItemNumber = document.querySelector('.pwd-constraints__item.number');
+elemPwdItemUpper = document.querySelector('.pwd-constraints__item.upper');
+elemPwdItemLower = document.querySelector('.pwd-constraints__item.lower');
+elemPwdItemSpecial = document.querySelector('.pwd-constraints__item.special');
 
 const handleInput = event => {
   const element = event.target;
+  console.log(element)
   if (!element.required && !element.value) element.classList.add('empty');
+  if (element === elemPassword) {
+    const pwd = element.value;
+    if (pwd.length >= 8) elemPwdItemLength.classList.add('valid');
+    else elemPwdItemLength.classList.remove('valid');
+
+    if (pwd.match(/\d/)) elemPwdItemNumber.classList.add('valid');
+    else elemPwdItemNumber.classList.remove('valid');
+
+    if (pwd.match(/\p{Lu}/gu)) elemPwdItemUpper.classList.add('valid');
+    else elemPwdItemUpper.classList.remove('valid');
+
+    if (pwd.match(/\p{Ll}/gu)) elemPwdItemLower.classList.add('valid');
+    else elemPwdItemLower.classList.remove('valid');
+
+    if (pwd.match(/[^\p{L}\d]/gu)) elemPwdItemSpecial.classList.add('valid');
+    else elemPwdItemSpecial.classList.remove('valid');
+
+    let valid = true;
+    for (const item of [
+      elemPwdItemLength,
+      elemPwdItemNumber,
+      elemPwdItemUpper,
+      elemPwdItemLower,
+      elemPwdItemSpecial
+    ]) {
+      if (!item.className.includes('valid')) {
+        valid = false;
+        element.setCustomValidity('Invalid password');
+        break;
+      };
+    }
+    if (valid) {
+      element.classList.remove('empty');
+      element.setCustomValidity('');
+    };
+
+  }
   if ([elemPassword, elemPasswordConfirm].includes(element)) {
-    if (elemPassword.value !== elemPasswordConfirm.value) {
-      elemPasswordConfirm.classList.add('invalid');
-    } else elemPasswordConfirm.classList.remove('invalid');
+    if (!elemPassword.validity.valid) {
+      elemPasswordConfirm.setCustomValidity(`Invalid password`);
+    } else if (elemPassword.value !== elemPasswordConfirm.value) {
+      elemPasswordConfirm.setCustomValidity(`Passwords don't match`);
+    } else {
+      elemPasswordConfirm.setCustomValidity('');
+      elemPasswordConfirm.classList.remove('empty');
+    };
   }
 }
 
 const handleFocusOut = event => {
   const element = event.target;
   if (element.required || element.value) element.classList.remove('empty');
+  // if (element === elemPassword) elemPasswordConstraints.classList.remove('enabled');
 }
 
+const handleFocusIn = event => {
+  const element = event.target;
+  if ([elemPassword, elemPasswordConfirm].includes(element)) elemPasswordConstraints.classList.add('enabled');
+}
+
+
+elemForm.addEventListener('focusin', handleFocusIn);
 elemForm.addEventListener('input', handleInput);
 elemForm.addEventListener('focusout', handleFocusOut);
